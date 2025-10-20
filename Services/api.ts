6fdf0e4e5eec2d0b2766d668884e091e1b80719a
kuki44 <ekramsiraj32@gmail.com>
2025-10-20 20:52:44 +0300

    fixed status text error

--- Services/api.ts
+++ Services/api.ts
@@ -8,15 +8,16 @@ export const TMDB_CONFIG = {
 }
 
 export const fetchMovies = async ({ query }: { query: string }) => {
-    const endpoint = '/discover/movie?sort_by=popularity.desc';
+    const endpoint = `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endpoint, {
         method: 'GET',
         headers: TMDB_CONFIG.headers,
-    })
+    });
 
-    if (!response.ok)
-        throw new Error('Failed to fetch movies', response.statusText);
+    if (!response.ok){
+        throw new Error(`Failed to fetch movies: ${response.status} ${response.statusText}`);
+    }
 
     const data = await response.json();
     return data.results;
