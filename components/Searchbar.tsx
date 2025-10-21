b194beb27f25448443001059ea99afe4293c114d
cook-iess <ekramsiraj32@gmail.com>
2025-10-21 11:10:39 +0300

    implemented serach screen a full working one

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -4,20 +4,22 @@ import { Image, TextInput, View } from 'react-native';
 
 interface Props {
     placeholder: string;
-    onPress?: () => void; 
+    onPress?: () => void;
+    value: string;
+    onChangeText: (text: string) => void;
 }
 
-const Searchbar = ({ placeholder, onPress}: Props) => {
+const Searchbar = ({ placeholder, onPress, value, onChangeText }: Props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
             <TextInput
-            onPress={onPress}
-            placeholder={placeholder}
-            value=''
-            onChangeText={()=>{}}
-            placeholderTextColor="#a8b5db"
-            className='flex-1 ml-2 text-white'
+                onPress={onPress}
+                placeholder={placeholder}
+                value={value}
+                onChangeText={onChangeText}
+                placeholderTextColor="#a8b5db"
+                className='flex-1 ml-2 text-white'
             />
         </View>
     )
